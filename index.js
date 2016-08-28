561357ab3d91ed199d4b24ae3158f9e56a43352f
Tara <tararee@gmail.com>
2016-08-28 13:38:58 -0400

    added server.js

--- index.js
+++ index.js
@@ -14,6 +14,6 @@ io.on('connection', function(socket){
   });
 });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+http.listen(8080, function(){
+  console.log('listening on *:8080');
 });
